58fd012b45a6e19da762d70808d4ecbba5a19b94
aplascenm <aplascenm@gmail.com>
2026-03-03 22:53:38 +0200

    Cleaner code and renamed variables for arp and dhcp

--- arp.h
+++ arp.h
@@ -7,106 +7,133 @@ using namespace std;
 
 void arp (const struct pcap_pkthdr *header, const u_char *buffer)
 {
-    unsigned char cb;
-    stringstream ss;
-    string s;
+    unsigned char current_byte;
+    stringstream packet_stream;
+    string packet_string;
+    
+    //Convert raw buffer to string
     for(unsigned int j=0; j<header->len; j++)
     {
-        ss<<buffer[j];
+        packet_stream<<buffer[j];
     }
-    s=ss.str();
+    
+    packet_string=packet_stream.str();
+    
     cout<<endl<<"                ARP                 "<<endl;
+    
     ///-------Hardware type------------------------
-    char *bin, *cc;
-    long int n;
-    string s2;
-    stringstream ss2,z;
-    cb=s[14];
-    bin=chartobin(cb);
-    ss2<<bin;
-    cb=s[15];
-    bin=chartobin(cb);
-    ss2<<bin;
-    s2=ss2.str();
-    n=strtoull(s2.c_str(), &cc, 2);
-    z<<n;
-    s2=z.str();
-    verificarHT(s2);
+    char *binary_ptr, *end_ptr;
+    long int hardware_type_decimal;
+    string binary_string;
+    stringstream binary_stream,decimal_stream;
+    
+    current_byte=packet_string[14];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+    
+    current_byte=packet_string[15];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+    
+    binary_string=binary_stream.str();
+    hardware_type_decimal=strtoull(binary_string.c_str(), &end_ptr, 2);
+    
+    decimal_stream<<hardware_type_decimal;
+    binary_string=decimal_stream.str();
+    
+    verificarHT(binary_string);
     //cout<<endl<<"Hardware Type: "<<s;
     /*cout<<endl<<"Hardware Type: "<<(int)ch[0];
     if((int)ch[0]==1)
     {
         cout<<" -> Ethernet";
     }*/
+    
     ///-------Protocol Type------------------------
-    stringstream s1;
-    cb=s[16];
-    s1<<hex<<setw(2)<<setfill('0')<<(unsigned int)cb;
-    cb=s[17];
-    s1<<hex<<setw(2)<<setfill('0')<<(unsigned int)cb;
-    s2="0x"+s1.str();
+    stringstream protocol_stream;
+    
+    current_byte=packet_string[16];
+    protocol_stream<<hex<<setw(2)<<setfill('0')<<(unsigned int)current_byte;
+    
+    current_byte=packet_string[17];
+    protocol_stream<<hex<<setw(2)<<setfill('0')<<(unsigned int)current_byte;
+    
+    binary_string="0x"+protocol_stream.str();
+    
     cout<<endl<<"Protocol Type: ";
-    verificardE(s2);
+    verificardE(binary_string);
+    
     //printf("0x%02x%02x", (int)ch[0], (int)ch[1]);
     ///-------Hardware Size------------------------
-    cb=s[18];
-    cout<<endl<<"Hardware Size: "<<(unsigned int)cb;
+    current_byte=packet_string[18];
+    cout<<endl<<"Hardware Size: "<<(unsigned int)current_byte;
+    
     ///-------Protocol Size------------------------
-    cb=s[19];
-    cout<<endl<<"Protocol Size: "<<(unsigned int)cb;
+    current_byte=packet_string[19];
+    cout<<endl<<"Protocol Size: "<<(unsigned int)current_byte;
+    
     ///-------OPCODE/request/reply-----------------
     cout<<endl<<"OPCODE: ";
-    cb=s[21];
-    if((unsigned int)cb==1)
+    current_byte=packet_string[21];
+    
+    if((unsigned int)current_byte==1)
     {
-        cout<<(unsigned int)cb<<" -> Request";
+        cout<<(unsigned int)current_byte<<" -> Request";
     }else
     {
-        cout<<(unsigned int)cb<<" -> Reply";
+        cout<<(unsigned int)current_byte<<" -> Reply";
     }
+    
     ///--------Sender Mac----------------------
     cout<<endl<<"Sender Mac: ";
     for(int j=22; j<28; j++)
     {
-        cb=s[j];
-        printf("%02x", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%02x", (unsigned int)current_byte);
+        
         if(j<27)
         {
             cout<<":";
         }
     }
+
     ///-----------Sender IP---------------------
     cout<<endl<<"Sender IP: ";
     for(int j=28; j<32; j++)
     {
-        cb=s[j];
-        printf("%d", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%d", (unsigned int)current_byte);
+       
         if(j<31)
         {
             cout<<".";
         }
     }
+
     ///-----------Tarjet MAC----------------------
     cout<<endl<<"Tarjet Mac: ";
     for(int j=32; j<38; j++)
     {
-        cb=s[j];
-        printf("%02x", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%02x", (unsigned int)current_byte);
         if(j<37)
         {
             cout<<":";
         }
     }
+
     ///------------Tarjet IP----------------------
     cout<<endl<<"Tarjet IP: ";
     for(int j=38; j<42; j++)
     {
-        cb=s[j];
-        printf("%d", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%d", (unsigned int)current_byte);
+        
         if(j<41)
         {
             cout<<".";
         }
     }
 }
+
 #endif // ARP_H_INCLUDED
--- dhcp.h
+++ dhcp.h
@@ -18,275 +18,331 @@ void fla(char *c)
     }
 }
 
-void dhcpop(string s, long int pos)
+void dhcpop(string packet_string, long int current_position)
 {
-    string s2;
-    char *bin, *cc;
-    unsigned char cb;
-    cb=s[pos];
-    unsigned int ui=(unsigned int)cb;
-    if(ui==255)
+    string value_string;
+    char *binary_ptr, *end_ptr;
+    unsigned char current_byte;
+    
+    current_byte=packet_string[current_position];
+    unsigned int option_code=(unsigned int)current_byte;
+    
+    if(option_code==255)
     {
         cout<<endl<<"Code: 255 -> Fin de Opciones.";
         return;
-    }else if(ui==4)
+    }
+    else if(option_code==4)
     {
         cout<<endl<<"Code: 4 -> Time Server Option.";
-        pos++;
+        current_position++;
+        
         //Len
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
-    }else if(ui==12)
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
+    }
+    else if(option_code==12)
     {
         cout<<endl<<"Code: 12 -> Host Name Option.";
-        pos++;
+        current_position++;
+        
         //Len
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
-    }else if(ui==15)
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
+    }
+    else if(option_code==15)
     {
         cout<<endl<<"Code: 15 -> Domain Name Option.";
-        pos++;
+        current_position++;
+        
         //Len
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
-    }else if(ui==51)
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
+    }
+    else if(option_code==51)
     {
         cout<<endl<<"Code: 51 -> IP Address Lease Time Option.";
-        pos++;
+        current_position++;
+
         //Len
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
+        
         //Lease Time
-        double nn;
-        long int n;
-        stringstream z;
-        string s2;
-        cb=s[pos];
-        pos++;
-        bin=chartobin(cb);
-        z<<bin;
-        cb=s[pos];
-        pos++;
-        bin=chartobin(cb);
-        z<<bin;
-        cb=s[pos];
-        pos++;
-        bin=chartobin(cb);
-        z<<bin;
-        cb=s[pos];
-        pos++;
-        bin=chartobin(cb);
-        z<<bin;
-        s2=z.str();
-        n=strtoull(s2.c_str(), &cc, 2);
-        nn=(n/60)/60;
-        cout<<endl<<"Lease Time: "<<n<<"S. ("<<nn<<" H.)";
-    }else if(ui==53)
+        double lease_time_hours;
+        long int lease_time_seconds;
+        stringstream binary_stream;
+        string binary_string;
+
+        current_byte=packet_string[current_position];
+        current_position++;
+        binary_ptr=chartobin(current_byte);
+        binary_stream<<binary_ptr;
+        
+        current_byte=packet_string[current_position];
+        current_position++;
+        binary_ptr=chartobin(current_byte);
+        binary_stream<<binary_ptr;
+        
+        current_byte=packet_string[current_position];
+        current_position++;
+        binary_ptr=chartobin(current_byte);
+        binary_stream<<binary_ptr;
+        
+        current_byte=packet_string[current_position];
+        current_position++;
+        binary_ptr=chartobin(current_byte);
+        binary_stream<<binary_ptr;
+
+        binary_string=binary_stream.str();
+        lease_time_seconds=strtoull(binary_string.c_str(), &end_ptr, 2);
+        lease_time_hours=(lease_time_seconds/60)/60;
+        
+        cout<<endl<<"Lease Time: "<<lease_time_seconds<<"S. ("<<lease_time_hours<<" H.)";
+    }
+    else if(option_code==53)
     {
         cout<<endl<<"Code: 53 -> DHCP Message Type Option.";
-        pos++;
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
-        cb=s[pos];
-        stringstream mt;
-        mt<<(unsigned int)cb;
-        s2=mt.str();
+        current_position++;
+        
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
+        
+        current_byte=packet_string[current_position];
+
+        stringstream message_type_stream;
+        message_type_stream<<(unsigned int)current_byte;
+        value_string=message_type_stream.str();
+        
         for(int j=0; j<9; j++)
         {
-            if(s2==dhcpmt[j])
+            if(value_string==dhcpmt[j])
             {
-                cout<<endl<<"Value: "<<s2<<" -> "<<dhcpmt[j+1];
+                cout<<endl<<"Value: "<<value_string<<" -> "<<dhcpmt[j+1];
             }
         }
-    }else if(ui==54)
+    }
+    else if(option_code==54)
     {
         cout<<endl<<"Code: 54 -> Server Identifier Option.";
-        pos++;
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
+        current_position++;
+        
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
+        
         cout<<endl<<"Address: ";
-        for(int j=pos; j<pos+4; j++)
+        for(int j=current_position; j<current_position+4; j++)
         {
-            cb=s[j];
-            printf("%d", (unsigned int)cb);
-            if(j<pos+3)
+            current_byte=packet_string[j];
+            printf("%d", (unsigned int)current_byte);
+            if(j<current_position+3)
             {
                 cout<<".";
             }
         }
-    }else if(ui==55)
+    }
+    else if(option_code==55)
     {
-        cout<<endl<<"Code: 54 -> Server Identifier Option.";
-        pos++;
-        cb=s[pos];
-        cout<<endl<<"LEN: "<<(unsigned int)cb;
-        pos++;
+        cout<<endl<<"Code: 55 -> Parameter Request List Option.";
+        current_position++;
+        
+        current_byte=packet_string[current_position];
+        cout<<endl<<"LEN: "<<(unsigned int)current_byte;
+        current_position++;
     }else
     {
         cout<<endl<<"No Encontrado";
         return;
     }
-    dhcpop(s, pos);
+
+    dhcpop(packet_string, current_position);
 }
 
-void dhcp(const struct pcap_pkthdr *header, const u_char *buffer, int tp)
+void dhcp(const struct pcap_pkthdr *packet_header, const u_char *packet_buffer, int transport_protocol)
 {
-    unsigned char cb;
-    stringstream ss;
-    string s;
-    for(unsigned int j=0; j<header->len; j++)
+    unsigned char current_byte;
+    stringstream packet_stream;
+    string packet_string;
+
+    for(unsigned int j=0; j<packet_header->len; j++)
     {
-            ss<<buffer[j];
+            packet_stream<<packet_buffer[j];
     }
-    s=ss.str();
+    packet_string=packet_stream.str();
+
     ///
-    char *bin;
+    char *binary_ptr;
     cout<<endl<<endl<<"                DHCP - IPV4                 "<<endl;
+    
     //Message Type
-    cb=s[42];
-    cout<<endl<<"Message Type: "<<(unsigned int)cb;if((unsigned int)cb==1){cout<<"-> Request";}else{"-> Reply";}
+    current_byte=packet_string[42];
+    cout<<endl<<"Message Type: "<<(unsigned int)current_byte;
+    if((unsigned int)current_byte==1){cout<<"-> Request";}else{"-> Reply";}
+    
     //Hardware Type
-    cb=s[43];
-    cout<<endl<<"Hardware Type: "<<(unsigned int)cb;if((unsigned int)cb==1){cout<<"-> Ethernet";}
+    current_byte=packet_string[43];
+    cout<<endl<<"Hardware Type: "<<(unsigned int)current_byte;
+    if((unsigned int)current_byte==1){cout<<"-> Ethernet";}
+    
     //Hardware Address Length
-    cb=s[44];
-    cout<<endl<<"Hardware Address Length: "<<(unsigned int)cb;
+    current_byte=packet_string[44];
+    cout<<endl<<"Hardware Address Length: "<<(unsigned int)current_byte;
+    
     //Hops
-    cb=s[45];
-    cout<<endl<<"Hops: "<<(unsigned int)cb;
+    current_byte=packet_string[45];
+    cout<<endl<<"Hops: "<<(unsigned int)current_byte;
+    
     //Transaction ID
-    long long int n;
-    stringstream z;
-    string s2;
-    char *cc;
-    cb=s[46];
-    bin=chartobin(cb);
-    z<<bin;
-    cb=s[47];
-    bin=chartobin(cb);
-    z<<bin;
-    cb=s[48];
-    bin=chartobin(cb);
-    z<<bin;
-    cb=s[49];
-    bin=chartobin(cb);
-    z<<bin;
-    s2=z.str();
-    n=strtoull(s2.c_str(), &cc, 2);
-    cout<<endl<<"Transaction ID: "<<n;
+    long long int transaction_id;
+    stringstream binary_stream;
+    string binary_string;
+    char *end_ptr;
+
+    current_byte=packet_string[46];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+    
+    current_byte=packet_string[47];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+    
+    current_byte=packet_string[48];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+    
+    current_byte=packet_string[49];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+    
+    binary_string=binary_stream.str();
+    transaction_id=strtoull(binary_string.c_str(), &end_ptr, 2);
+    cout<<endl<<"Transaction ID: "<<transaction_id;
+
     //Seconds Elapsed
-    stringstream z1;
-    cb=s[50];
-    bin=chartobin(cb);
-    z<<bin;
-    cb=s[51];
-    bin=chartobin(cb);
-    z<<bin;
-    s2=z.str();
-    n=strtoull(s2.c_str(), &cc, 2);
-    cout<<endl<<"Seconds Elapsed: "<<n;
+    stringstream seconds_stream;
+
+    current_byte=packet_string[50];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+
+    current_byte=packet_string[51];
+    binary_ptr=chartobin(current_byte);
+    binary_stream<<binary_ptr;
+
+    binary_string=binary_stream.str();
+    transaction_id=strtoull(binary_string.c_str(), &end_ptr, 2);
+    cout<<endl<<"Seconds Elapsed: "<<transaction_id;
+
     //Flag(s)
-    cb=s[52];
-    bin=chartobin(cb);
-    fla(bin);
+    current_byte=packet_string[52];
+    binary_ptr=chartobin(current_byte);
+    fla(binary_ptr);
+
     //Client Address
     cout<<endl<<"Client Address: ";
     for(int j=54; j<58; j++)
     {
-        cb=s[j];
-        printf("%d", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%d", (unsigned int)current_byte);
         if(j<57)
         {
             cout<<".";
         }
     }
+
     //Your Client Address
     cout<<endl<<"Your Client Address: ";
     for(int j=58; j<62; j++)
     {
-        cb=s[j];
-        printf("%d", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%d", (unsigned int)current_byte);
         if(j<61)
         {
             cout<<".";
         }
     }
+
     //Next Server IP
     cout<<endl<<"Next Server IP: ";
     for(int j=62; j<66; j++)
     {
-        cb=s[j];
-        printf("%d", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%d", (unsigned int)current_byte);
         if(j<65)
         {
             cout<<".";
         }
     }
+
     //Relay Agent IP
     cout<<endl<<"Relay Agent IP: ";
     for(int j=66; j<70; j++)
     {
-        cb=s[j];
-        printf("%d", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%d", (unsigned int)current_byte);
         if(j<69)
         {
             cout<<".";
         }
     }
+
     //Client Mac Address
     cout<<endl<<"Client Mac Address: ";
     for(int j=70; j<76; j++)
     {
-        cb=s[j];
-        printf("%02x", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%02x", (unsigned int)current_byte);
         if(j<75)
         {
             cout<<":";
         }
     }
+
     //10 Bytes Padding
     //Server Host Name
     cout<<endl<<"Server Host Name: ";
     for(int j=86; j<150; j++)
     {
-        cb=s[j];
-        printf("%02x", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%02x", (unsigned int)current_byte);
         if(j<149)
         {
             cout<<" ";
         }
     }
+
     //Boot File
     cout<<endl<<"Boot File: ";
     for(int j=150; j<278; j++)
     {
-        cb=s[j];
-        printf("%02x", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%02x", (unsigned int)current_byte);
         if(j<277)
         {
             cout<<" ";
         }
     }
+
     //Magic Cookie
     cout<<endl<<"Magic Cookie: ";
     for(int j=278; j<282; j++)
     {
-        cb=s[j];
-        printf("%02x", (unsigned int)cb);
+        current_byte=packet_string[j];
+        printf("%02x", (unsigned int)current_byte);
         if(j<281)
         {
             cout<<" ";
         }
     }
+    
     //Options
-    dhcpop(s, 282);
+    dhcpop(packet_string, 282);
 }
 
 #endif // DHCP_H_INCLUDED
